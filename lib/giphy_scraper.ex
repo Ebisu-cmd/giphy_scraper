f6d7dd17780d1ecaaebf63bd8bc94e946e55fe69
Gabriel Jardio <gabejardio@gmail.com>
2022-04-23 21:42:46 -0500

    Added url construction function and get request function

--- lib/giphy_scraper.ex
+++ lib/giphy_scraper.ex
@@ -1,5 +1,25 @@
 defmodule GiphyScraper do
+
   def search(query) do
+    query
+    |> construct_GIPHY_search_url_endpoint
+    |> issue_get_request
+    |> process_get_response
+  end
+
+  defp construct_GIPHY_search_url_endpoint(query) do
+    "api.giphy.com/v1/gifs/search?api_key=1CQoxaod6AB5n4ttMEFd9HhqlGkjzvdt&limit=25&q=" <> query
+  end
+
+  defp issue_get_request(url) do
+    case HTTPoison.get(url) do
+      {:ok, response} -> response.body
+      #add processing for get request errors {:error, error}
+    end
+  end
+
+  defp process_get_response(body) do
 
   end
+
 end
