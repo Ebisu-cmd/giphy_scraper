42582538d861f42eb6a77033a1ead4cdd38a7e65
Gabriel Jardio <gabejardio@gmail.com>
2022-04-24 01:12:28 -0500

    Implemented functions for processing get response

--- lib/giphy_scraper.ex
+++ lib/giphy_scraper.ex
@@ -1,14 +1,13 @@
 defmodule GiphyScraper do
-
   def search(query) do
     query
-    |> construct_GIPHY_search_url_endpoint
+    |> construct_giphy_search_url_endpoint
     |> issue_get_request
     |> process_get_response
   end
 
-  defp construct_GIPHY_search_url_endpoint(query) do
-    "api.giphy.com/v1/gifs/search?api_key=1CQoxaod6AB5n4ttMEFd9HhqlGkjzvdt&limit=1&q=" <> query
+  defp construct_giphy_search_url_endpoint(query) do
+    "api.giphy.com/v1/gifs/search?api_key=1CQoxaod6AB5n4ttMEFd9HhqlGkjzvdt&limit=25&q=" <> query
   end
 
   defp issue_get_request(url) do
@@ -16,7 +15,11 @@ defmodule GiphyScraper do
   end
 
   defp process_get_response(response) do
-    response.body
+    parsed_body = Jason.decode!(response.body, [{:keys, :atoms}])
+    Enum.map(parsed_body.data, &parse_gif_object/1)
   end
 
+  defp parse_gif_object(gif_object) do
+    %GiphyImage{id: gif_object[:id], url: gif_object[:url], username: gif_object[:username], title: gif_object[:title]}
+  end
 end
